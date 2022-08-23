fdfe86f95a7f66ce9079d6791a55cc025c6c91f8
Liluter <karolawdziewicz@gmail.com>
2022-08-24 01:02:25 +0200

    Change folder name

--- src/features/Summary/Summary.js
+++ src/features/Summary/Summary.js
@@ -7,25 +7,25 @@ const {Header,Content} = Layout
 const {Title, Text} = Typography
 
 export const Summary = ({seats}) => {
-    const Seats = useSelector(selectAllSeats)
-    return (
-    <>
-        <Header  >
-        <Text style={{color:"#ddd", fontSize:"24px"}}>Your reservation was successful.</Text>
-        </Header>
-        <Content >
-            <Row justify="center">
-                <Col span={22}>
-                        <List 
-                            size="large"
-                            footer={<Title level={4} >Thank you! If you have any problems, please contact the administration department.</Title>}
-                            dataSource={Seats.filter((seat) => (seat.myRes)).map(seat => (`- row: ${seat.cords.x} , seat: ${seat.cords.y} ,   ${seat.id}`))}
-                            renderItem={(item=>(<List.Item>{item}</List.Item>))}
-                            >
-                        </List>
-                </Col>
-            </Row>
-        </Content>
-    </>
-    )
+	const Seats = useSelector(selectAllSeats)
+	return (
+	<>
+		<Header  >
+		<Text style={{color:"#ddd", fontSize:"24px"}}>Your reservation was successful.</Text>
+		</Header>
+		<Content >
+			<Row justify="center">
+				<Col span={22}>
+					<List 
+						size="large"
+						footer={<Title level={4} >Thank you! If you have any problems, please contact the administration department.</Title>}
+						dataSource={Seats.filter((seat) => (seat.myRes)).map(seat => (`- row: ${seat.cords.x} , seat: ${seat.cords.y} ,   ${seat.id}`))}
+						renderItem={(item=>(<List.Item>{item}</List.Item>))}
+						>
+					</List>
+				</Col>
+			</Row>
+		</Content>
+	</>
+	)
 }
